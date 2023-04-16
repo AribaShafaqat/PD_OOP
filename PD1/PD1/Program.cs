89bab05e2877cec0755ae6f9d15ff2b6cfd7ed1b
Ariba Shafaqat <aribashafaqat4@gmail.com>
2023-04-16 20:12:32 +0500

    oop

---
+++ PD1/PD1/Program.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PD1
+{
+    internal class Program
+    {
+    
+            static void Main(string[] args)
+            {
+
+                string[] plant = new string[10];
+                int[] price = new int[10];
+                int[] quantity = new int[10];
+                int Count = 0;
+
+
+                string inventoryPath = "C:\\Users\\dell\\Documents\\OOP\\week\\Week1\\Week1\\data.txt.txt";
+
+
+                readPlanttData(inventoryPath, plant, price, ref Count);
+                char option;
+                do
+                {
+
+                    option = Owner_menu();
+                if(option=='1')
+                {
+                    viewPlants(plant, price, Count);
+                }
+                    if (option == '2')
+                    {
+                        Console.WriteLine("Enter Plant Name: ");
+                        string newPlant = Console.ReadLine();
+                        Console.WriteLine("Enter Price: ");
+                        int newPrice = int.Parse(Console.ReadLine());
+
+                        addItem(newPlant, newPrice, plant, price, ref Count);
+                    }
+                    if (option == '3')
+                    {
+                        viewPlants(plant, price, Count);
+                        Console.WriteLine("Enter Plant Name you want to Update: ");
+                        string newPlant = Console.ReadLine();
+                        updatePlant(newPlant, plant, price, ref Count);
+                    }
+                    if (option == '4')
+                    {
+                        viewPlants(plant, price, Count);
+                        Console.WriteLine("Enter Plant Name you want to Delete: ");
+                        string newPlant = Console.ReadLine();
+                        deletePlant(newPlant, plant, price, ref Count);
+                    }
+                    savePlantData(inventoryPath, plant, price, Count);
+                } while (option != '5');
+
+
+            }
+            static char Owner_menu()
+            {
+                Console.Clear();
+                char option;
+                Console.WriteLine("Press 1 to View Stock:");
+                Console.WriteLine("Press 2 to Add a New Plant:");
+                Console.WriteLine("Press 3 to Update an Exisitng Plant: ");
+                Console.WriteLine("Press 4 to Delete a Plant:");
+                Console.WriteLine("Press 5 to Exit: ");
+                Console.WriteLine("Enter Option: ");
+                option = char.Parse(Console.ReadLine());
+                return option;
+            }
+            static void addItem(string newPlant, int newPrice, string[] plant, int[] price, ref int Count)
+            {
+                if (Count <= 9)
+                {
+                    plant[Count] = newPlant;
+                    price[Count] = newPrice;
+                    Count++;
+                }
+            }
+            static void viewPlants(string[] plant, int[] price, int Count)
+            {
+                Console.WriteLine("Plant\tPrice");
+                for (int x = 0; x < Count; x++)
+                {
+                    Console.WriteLine(plant[x] + "\t" + price[x]);
+                }
+            }
+            static int findPlant(string newPlant, string[] plant, int Count)
+            {
+                for (int x = 0; x < Count; x++)
+                {
+                    if (newPlant == plant[x])
+                    {
+                        return x;
+                    }
+                }
+                return -1;
+            }
+            static void clearScreen()
+            {
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+                Console.Clear();
+            }
+            static void updatePlant(string newPlant, string[] plant, int[] price, ref int Count)
+            {
+                int idx = findPlant(newPlant, plant, Count);
+                if (idx >= 0 && idx <= 9)
+                {
+                    Console.WriteLine("Plant Found");
+                    Console.WriteLine("Enter the Updated price");
+                    int newPrice = int.Parse(Console.ReadLine());
+                    price[idx] = newPrice;
+
+                }
+                else
+                {
+                    clearScreen();
+                    Console.WriteLine("Plant Does Not Exists.");
+                }
+            }
+            static void deletePlant(string newPlant, string[] plant, int[] price, ref int Count)
+            {
+                int idx = findPlant(newPlant, plant, Count);
+                if (idx >= 0 && idx <= 9)
+                {
+                    Console.WriteLine("Plant Found");
+                    for (int x = idx; x <Count - 1; x++)
+                    {
+                        plant[x] = plant[x + 1];
+                        price[x] = price[x + 1];
+
+                    }
+                    Count++;
+                }
+            }
+            static void savePlantData(string inventoryPath, string[] plant, int[] price, int Count)
+            {
+                StreamWriter f1 = new StreamWriter(inventoryPath, false);
+                for (int x = 0; x < Count; x++)
+                {
+                    f1.WriteLine(plant[x] + "," + price[x]);
+                }
+                f1.Flush();
+                f1.Close();
+            }
+            static string parseData(string record, int field)
+            {
+                int comma = 1;
+                string item = "";
+                for (int x = 0; x < record.Length; x++)
+                {
+                    if (record[x] == ',')
+                    {
+                        comma++;
+                    }
+                    else if (comma == field)
+                    {
+                        item = item + record[x];
+                    }
+                }
+                return item;
+            }
+            static void readPlanttData(string inventoryPath, string[] plant, int[] price, ref int Count)
+            {
+                StreamReader fp = new StreamReader(inventoryPath);
+                string record;
+                while ((record = fp.ReadLine()) != null)
+                {
+                    if (Count > 9)
+                    {
+                        break;
+                    }
+                    plant[Count] = parseData(record, 1);
+                    price[Count] = int.Parse(parseData(record, 2));
+                    Count++;
+                }
+                fp.Close();
+            }
+
+        }
+    }
+
+
+
+
---
+++ PD2/project/project/Program.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project
+{
+    internal class Program
+    {
+        class Owner
+        {
+            public string plant;
+            public int price;
+        }
+        static void Main(string[] args)
+        {
+            List<Owner> data = new List<Owner>();
+            
+
+            string inventoryPath = "C:\\Users\\dell\\Documents\\OOP\\week\\Week1\\.vs\\file.txt.txt";
+            readplantData(inventoryPath, data);
+            char option;
+            do
+            {
+
+                option = ownerMenu();
+                if (option =='1')
+                {
+                    viewstock(data);
+                    Console.ReadKey();
+                }
+                if (option == '2')
+                {
+                    Console.WriteLine("Enter plant Name: ");
+                    string newplant = Console.ReadLine();
+                    Console.WriteLine("Enter Price: ");
+                    int newPrice = int.Parse(Console.ReadLine());
+                    add(newplant, newPrice, data);
+                }
+                if (option == '3')
+                {
+                    viewstock(data);
+                    Console.WriteLine("Enter Plant Name you want to Delete: ");
+                    string newplant = Console.ReadLine();
+                    deleteplant(newplant, data);
+
+                }
+                if (option == '4')
+                {
+                    viewstock(data);
+                    Console.WriteLine("Enter Product Name you want to Update: ");
+                    string newplant = Console.ReadLine();
+                    update(newplant, data);
+                }
+                saveplantData(inventoryPath, data);
+            }
+            while (option!=5);
+        }
+
+
+
+        static char ownerMenu()
+        {
+            Console.Clear();
+            char option;
+            Console.WriteLine("1. View Stock ");
+            Console.WriteLine("2. Add a plant ");
+            Console.WriteLine("3. Delete a plant ");
+            Console.WriteLine("4. Update Price");
+            option=Char.Parse(Console.ReadLine());
+            return option;
+
+        }
+        static void viewstock(List<Owner> data)
+        {
+            Owner info = new Owner();
+            Console.WriteLine("Plant\t Price");
+            for (int x = 0; x < data.Count; x++)
+            {
+                Console.WriteLine(info.plant + "\t" + info.price);
+            }
+        }
+        static void add(string newplant, int newprice, List<Owner> data)
+        {
+            Owner info = new Owner();
+ 
+                info.plant=newplant;
+                info.price=newprice;
+                data.Add(info);
+            
+        }
+        static int findplant(string newplant, List<Owner> data)
+        {
+            Owner info = new Owner();
+            for (int x = 0; x < data.Count; x++)
+            {
+                if (newplant == info.plant)
+                {
+                    return x;
+                }
+            }
+            return -1;
+        }
+        static void clearScreen()
+        {
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+        static void deleteplant(string newplant, List<Owner> data)
+        {
+            Owner info = new Owner();
+            int idx = findplant(newplant, data);
+            if (idx >= 0 && idx <= 9)
+            {
+                Console.WriteLine("Plant Found");
+                data.RemoveAt(idx);
+            }
+        }
+        static void update(string newplant, List<Owner> data)
+        {
+            Owner info = new Owner();
+            int idx = findplant(newplant, data);
+            if (idx >= 0 && idx <= 9)
+            {
+                Console.WriteLine("Plant Found");
+                Console.WriteLine("Enter the Updated price");
+                int newPrice = int.Parse(Console.ReadLine());
+                info.price = newPrice;
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Plant Does Not Exists.");
+            }
+        }
+
+    
+
+        
+        static void saveplantData(string inventoryPath, List<Owner> data)
+        {
+            StreamWriter f1 = new StreamWriter(inventoryPath, false);
+            Owner info = new Owner();
+            for (int x = 0; x < data.Count; x++)
+            {
+                f1.WriteLine(info.plant + "," + info.price + ",");
+            }
+            f1.Flush();
+            f1.Close();
+        }
+        static string parseData(string record, int field)
+        {
+            int comma = 1;
+            string item = "";
+            for (int x = 0; x < record.Length; x++)
+            {
+                if (record[x] == ',')
+                {
+                    comma++;
+                }
+                else if (comma == field)
+                {
+                    item = item + record[x];
+                }
+            }
+            return item;
+        }
+        static void readplantData(string inventoryPath, List<Owner> data)
+        {
+            StreamReader fp = new StreamReader(inventoryPath);
+            string record;
+            while ((record = fp.ReadLine()) != null)
+            {
+                
+                Owner info = new Owner();
+                info.plant = parseData(record, 1);
+                info.price = int.Parse(parseData(record, 2));
+                data.Add(info);
+            }
+            fp.Close();
+        }
+    }
+}
